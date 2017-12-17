01040fd06d1e089c294931387e45c166e837d87c
Grant Holle <hollegrant@gmail.com>
2017-12-17 16:01:10 +0800

    Fix font face format property

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@ export default {
   font-family: 'Inconsolata';
   font-style: normal;
   font-weight: 400;
-  src: local('Inconsolata Regular'), local('Inconsolata-Regular'), url(/static/Inconsolata-Regular.woff2) format('ttf');
+  src: local('Inconsolata Regular'), local('Inconsolata-Regular'), url(/static/Inconsolata-Regular.woff2) format('woff2');
   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2212, U+2215;
 }
 
