2b762173f45f51076d40f690ec94036679cf8272
Grant Holle <hollegrant@gmail.com>
2017-12-18 21:30:29 +0800

    Add fun animation for reboot

--- src/components/Cli.vue
+++ src/components/Cli.vue
@@ -2,14 +2,14 @@
   <div class="cli" @keydown="handleInput" @click="handleClick">
     <input type="text" v-model="userInput" @blur="blur" autofocus spellcheck="false">
 
-    <div v-if="!rebooted">
-      <div>&nbsp;__&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;__&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;___</div>
-      <div>/\&nbsp;\&nbsp;&nbsp;__/\&nbsp;\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/\_&nbsp;\</div>
-      <div>\&nbsp;\&nbsp;\/\&nbsp;\&nbsp;\&nbsp;\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;__\//\&nbsp;\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;___&nbsp;&nbsp;&nbsp;&nbsp;___&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;___&nbsp;___&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;__</div>
-      <div>&nbsp;\&nbsp;\&nbsp;\&nbsp;\&nbsp;\&nbsp;\&nbsp;\&nbsp;&nbsp;/'__`\\&nbsp;\&nbsp;\&nbsp;&nbsp;&nbsp;/'___\&nbsp;/&nbsp;__`\&nbsp;/'&nbsp;__`&nbsp;__`\&nbsp;&nbsp;/'__`\</div>
-      <div>&nbsp;&nbsp;\&nbsp;\&nbsp;\_/&nbsp;\_\&nbsp;\/\&nbsp;&nbsp;__/&nbsp;\_\&nbsp;\_/\&nbsp;\__//\&nbsp;\L\&nbsp;\/\&nbsp;\/\&nbsp;\/\&nbsp;\/\&nbsp;&nbsp;__/</div>
-      <div>&nbsp;&nbsp;&nbsp;\&nbsp;`\___x___/\&nbsp;\____\/\____\&nbsp;\____\&nbsp;\____/\&nbsp;\_\&nbsp;\_\&nbsp;\_\&nbsp;\____\</div>
-      <div>&nbsp;&nbsp;&nbsp;&nbsp;'\/__//__/&nbsp;&nbsp;\/____/\/____/\/____/\/___/&nbsp;&nbsp;\/_/\/_/\/_/\/____/</div>
+    <div v-if="!clearScreen">
+      <div :class="{ rainbow: rebooted }">&nbsp;__&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;__&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;___</div>
+      <div :class="{ rainbow: rebooted }">/\&nbsp;\&nbsp;&nbsp;__/\&nbsp;\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/\_&nbsp;\</div>
+      <div :class="{ rainbow: rebooted }">\&nbsp;\&nbsp;\/\&nbsp;\&nbsp;\&nbsp;\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;__\//\&nbsp;\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;___&nbsp;&nbsp;&nbsp;&nbsp;___&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;___&nbsp;___&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;__</div>
+      <div :class="{ rainbow: rebooted }">&nbsp;\&nbsp;\&nbsp;\&nbsp;\&nbsp;\&nbsp;\&nbsp;\&nbsp;&nbsp;/'__`\\&nbsp;\&nbsp;\&nbsp;&nbsp;&nbsp;/'___\&nbsp;/&nbsp;__`\&nbsp;/'&nbsp;__`&nbsp;__`\&nbsp;&nbsp;/'__`\</div>
+      <div :class="{ rainbow: rebooted }">&nbsp;&nbsp;\&nbsp;\&nbsp;\_/&nbsp;\_\&nbsp;\/\&nbsp;&nbsp;__/&nbsp;\_\&nbsp;\_/\&nbsp;\__//\&nbsp;\L\&nbsp;\/\&nbsp;\/\&nbsp;\/\&nbsp;\/\&nbsp;&nbsp;__/</div>
+      <div :class="{ rainbow: rebooted }">&nbsp;&nbsp;&nbsp;\&nbsp;`\___x___/\&nbsp;\____\/\____\&nbsp;\____\&nbsp;\____/\&nbsp;\_\&nbsp;\_\&nbsp;\_\&nbsp;\____\</div>
+      <div :class="{ rainbow: rebooted }">&nbsp;&nbsp;&nbsp;&nbsp;'\/__//__/&nbsp;&nbsp;\/____/\/____/\/____/\/___/&nbsp;&nbsp;\/_/\/_/\/_/\/____/</div>
       <div>&nbsp;</div>
       <div>Welcome&nbsp;to&nbsp;GrantOS&nbsp;1.0.0&nbsp;LTS&nbsp;({{ userAgent }})</div>
       <div>&nbsp;</div>
@@ -22,29 +22,7 @@
       <div>0&nbsp;updates&nbsp;are&nbsp;security&nbsp;updates.</div>
       <div>&nbsp;</div>
       <div>&nbsp;</div>
-      <div>***&nbsp;System&nbsp;restart&nbsp;required&nbsp;***</div>
-    </div>
-
-    <div v-if="rebooted">
-      <div>&nbsp;__&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;__&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;___</div>
-      <div>/\&nbsp;\&nbsp;&nbsp;__/\&nbsp;\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/\_&nbsp;\</div>
-      <div>\&nbsp;\&nbsp;\/\&nbsp;\&nbsp;\&nbsp;\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;__\//\&nbsp;\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;___&nbsp;&nbsp;&nbsp;&nbsp;___&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;___&nbsp;___&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;__</div>
-      <div>&nbsp;\&nbsp;\&nbsp;\&nbsp;\&nbsp;\&nbsp;\&nbsp;\&nbsp;&nbsp;/'__`\\&nbsp;\&nbsp;\&nbsp;&nbsp;&nbsp;/'___\&nbsp;/&nbsp;__`\&nbsp;/'&nbsp;__`&nbsp;__`\&nbsp;&nbsp;/'__`\</div>
-      <div>&nbsp;&nbsp;\&nbsp;\&nbsp;\_/&nbsp;\_\&nbsp;\/\&nbsp;&nbsp;__/&nbsp;\_\&nbsp;\_/\&nbsp;\__//\&nbsp;\L\&nbsp;\/\&nbsp;\/\&nbsp;\/\&nbsp;\/\&nbsp;&nbsp;__/</div>
-      <div>&nbsp;&nbsp;&nbsp;\&nbsp;`\___x___/\&nbsp;\____\/\____\&nbsp;\____\&nbsp;\____/\&nbsp;\_\&nbsp;\_\&nbsp;\_\&nbsp;\____\</div>
-      <div>&nbsp;&nbsp;&nbsp;&nbsp;'\/__//__/&nbsp;&nbsp;\/____/\/____/\/____/\/___/&nbsp;&nbsp;\/_/\/_/\/_/\/____/</div>
-      <div>&nbsp;</div>
-      <div>Welcome&nbsp;to&nbsp;GrantOS&nbsp;1.0.1&nbsp;LTS&nbsp;({{ userAgent }})</div>
-      <div>&nbsp;</div>
-      <div>&nbsp;*&nbsp;Github:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a target="_blank" :href="links.gh">{{ links.gh }}</a></div>
-      <div>&nbsp;*&nbsp;Twitter:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a target="_blank" :href="links.twitter">{{ links.twitter }}</a></div>
-      <div>&nbsp;*&nbsp;npm:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a target="_blank" :href="links.npm">{{ links.npm }}</a></div>
-      <div>&nbsp;*&nbsp;Stackoverflow:&nbsp;&nbsp;&nbsp;&nbsp;<a target="_blank" :href="links.so">{{ links.so }}</a></div>
-      <div>&nbsp;</div>
-      <div>0&nbsp;packages&nbsp;can&nbsp;be&nbsp;updated.</div>
-      <div>0&nbsp;updates&nbsp;are&nbsp;security&nbsp;updates.</div>
-      <div>&nbsp;</div>
-      <div>&nbsp;</div>
+      <div v-if="!rebooted">***&nbsp;System&nbsp;restart&nbsp;required&nbsp;***</div>
     </div>
 
     <div v-for="(line, index) in lineFeed" :key="index">
@@ -95,10 +73,12 @@ export default {
       userAgent: window.navigator.userAgent,
       allowInput: false,
       rebooted: false,
+      clearScreen: false,
       commands: [
         'help',
         'reboot',
-        'hello'
+        'hello',
+        'social'
       ],
       commandHistoryIndex: 1,
       commandHistory: [
@@ -108,7 +88,8 @@ export default {
   },
   methods: {
     async getIp () {
-      const { data } = await axios.get(`https://api.ipify.org?format=json`)
+      // const { data } = await axios.get(`https://api.ipify.org?format=json`)
+      const data = { ip: '192.168.1.1' }
 
       this.lineFeed.push({
         text: `Current Login: ${moment().format('ddd MMM DD HH:mm:ss YYYY')} from ${data.ip}`
@@ -131,7 +112,7 @@ export default {
           if (command.length === 1) {
             this.userInput = command + ' '
           } else if (command.length > 1) {
-            this.preLineFeed = command.join(' ')
+            this.preLineFeed = command.join('&nbsp;&nbsp;')
           }
 
           this.resume()
@@ -184,6 +165,7 @@ export default {
           break
         default:
           this.resume()
+          this.handleClick()
           this.cursorOffset = this.cursorOffset < -8 ? this.cursorOffset : -8
           break
       }
@@ -222,13 +204,47 @@ export default {
     resume () {
       setTimeout(() => this.typing = false, 150)
     },
+
     // All the command functions
+    help (args) {
+      this.lineFeed.push({
+        text: 'Available commands:'
+      }, {
+        text: this.commands.sort().join(', ')
+      })
+
+      this.resume()
+    },
     hello (args) {
       this.lineFeed.push({
         text: `你好！`
       })
 
       this.resume()
+    },
+    reboot (args) {
+      this.allowInput = false
+
+      this.lineFeed.push({
+        text: `Rebooting... now!`
+      })
+
+      // Nest a bunch of timeouts to simulate loading
+      setTimeout(() => {
+        this.lineFeed = []
+        this.clearScreen = true
+
+        setTimeout(() => {
+          this.rebooted = true
+          this.clearScreen = false
+
+          this.getIp()
+          setTimeout(() => {
+            this.allowInput = true
+            this.resume()
+          }, 750)
+        }, 2000)
+      }, 2000)
     }
   }
 }
@@ -246,6 +262,110 @@ $black: #002833;
   50% {
     background: transparent;
   }
+}
+
+@keyframes rainbow {
+  0% { color: #F3801F; }
+  1% { color: #EF871A; }
+  2% { color: #EC8F16; }
+  3% { color: #E79712; }
+  4% { color: #E29F0E; }
+  5% { color: #DDA60B; }
+  6% { color: #D8AE08; }
+  7% { color: #D2B505; }
+  8% { color: #CCBC03; }
+  9% { color: #C5C302; }
+  10% { color: #BFCA01; }
+  11% { color: #B8D001; }
+  12% { color: #B0D601; }
+  13% { color: #A9DB01; }
+  14% { color: #A1E102; }
+  15% { color: #9AE604; }
+  16% { color: #92EA06; }
+  17% { color: #8AEE08; }
+  18% { color: #82F20B; }
+  19% { color: #7AF50E; }
+  20% { color: #72F812; }
+  21% { color: #6BFA16; }
+  22% { color: #63FC1B; }
+  23% { color: #5BFD20; }
+  24% { color: #54FE25; }
+  25% { color: #4CFE2B; }
+  26% { color: #45FE31; }
+  27% { color: #3EFE37; }
+  28% { color: #38FD3E; }
+  29% { color: #31FB45; }
+  30% { color: #2BF94C; }
+  31% { color: #26F653; }
+  32% { color: #20F35B; }
+  33% { color: #1BF063; }
+  34% { color: #16EC6A; }
+  35% { color: #12E872; }
+  36% { color: #0EE37A; }
+  37% { color: #0BDE82; }
+  38% { color: #08D98A; }
+  39% { color: #06D392; }
+  40% { color: #04CD99; }
+  41% { color: #02C6A1; }
+  42% { color: #01C0A9; }
+  43% { color: #01B9B0; }
+  44% { color: #01B1B7; }
+  45% { color: #01AABE; }
+  46% { color: #02A3C5; }
+  47% { color: #039BCB; }
+  48% { color: #0593D2; }
+  49% { color: #088BD7; }
+  50% { color: #0A83DD; }
+  51% { color: #0E7CE2; }
+  52% { color: #1174E7; }
+  53% { color: #166CEB; }
+  54% { color: #1A64EF; }
+  55% { color: #1F5CF3; }
+  56% { color: #2455F6; }
+  57% { color: #2A4EF9; }
+  58% { color: #3046FB; }
+  59% { color: #363FFC; }
+  60% { color: #3D39FE; }
+  61% { color: #4432FE; }
+  62% { color: #4B2CFE; }
+  63% { color: #5226FE; }
+  64% { color: #5A21FD; }
+  65% { color: #611CFC; }
+  66% { color: #6917FA; }
+  67% { color: #7113F8; }
+  68% { color: #790FF6; }
+  69% { color: #810CF2; }
+  70% { color: #8909EF; }
+  71% { color: #9006EB; }
+  72% { color: #9804E6; }
+  73% { color: #A002E2; }
+  74% { color: #A701DC; }
+  75% { color: #AF01D7; }
+  76% { color: #B601D1; }
+  77% { color: #BD01CB; }
+  78% { color: #C402C4; }
+  79% { color: #CA03BE; }
+  80% { color: #D105B7; }
+  81% { color: #D707AF; }
+  82% { color: #DC0AA8; }
+  83% { color: #E10DA0; }
+  84% { color: #E61199; }
+  85% { color: #EB1591; }
+  86% { color: #EF1989; }
+  87% { color: #F21E81; }
+  88% { color: #F52479; }
+  89% { color: #F82971; }
+  90% { color: #FA2F6A; }
+  91% { color: #FC3562; }
+  92% { color: #FD3C5A; }
+  93% { color: #FE4353; }
+  94% { color: #FE4A4B; }
+  95% { color: #FE5144; }
+  96% { color: #FE593D; }
+  97% { color: #FC6037; }
+  98% { color: #FB6830; }
+  99% { color: #F9702A; }
+  100% { color: #F67825; }
 }
 
 body {
@@ -294,6 +414,10 @@ body {
   &.blink {
     animation: 1s blink step-end infinite;
   }
+}
+
+.rainbow {
+  animation: 1s ease-in 1s infinite rainbow;
 }
 
 .user-prompt {
